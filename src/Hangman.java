b2ff9ce031127ad6cbbf839f29cea126738756ab
SofiMeza <meza8@purdue.edu>
2022-05-10 23:53:22 +0200

    Initial commit

---
+++ src/Hangman.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class Hangman {
+    public static void main(String[] args) {
+        char guess = 0;
+        boolean keepGoing = true;
+        boolean isInWord = false;
+        boolean isVerified = true;
+        int lives = 5;
+        int rightCount = 0;
+        StringBuilder guessedWord = new StringBuilder();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Insert string  for your friend to guess: ");
+        final String wordToGuess = scanner.nextLine();
+
+        for (int i = 0; i < 50; i++) {
+            System.out.println("*****************************************");
+        }
+        System.out.println();
+        System.out.println("Welcome to Hangman non GUI version !!!");
+        System.out.println("You have 5 lives ! Don't kill the lil man");
+        System.out.println();
+
+        for (int i = 0; i < wordToGuess.length(); i++) {
+            guessedWord.append("_");
+        }
+
+        System.out.println(guessedWord.toString());
+        System.out.println();
+
+        while(keepGoing) {
+            isVerified = true;
+            isInWord = false;
+            while (isVerified) {
+                System.out.print("Enter the character you want to guess: ");
+                String verificationTemp = scanner.nextLine();
+                System.out.println();
+
+                if (verificationTemp.length() != 1) {
+                    System.out.println("Error, please make sure you only enter one character");
+                } else {
+                    guess = verificationTemp.charAt(0);
+                    isVerified = false;
+                }
+            }
+
+            for (int i = 0; i < wordToGuess.length(); i++) {
+                if (wordToGuess.charAt(i) == guess) {
+                    guessedWord.setCharAt(i, guess);
+                    rightCount++;
+                    isInWord = true;
+                }
+            }
+
+            System.out.println(guessedWord.toString());
+            System.out.println();
+
+            if (!isInWord) {
+                lives--;
+                if (lives <= 0) {
+                    System.out.println("You killed lil guy !!!!");
+                    System.out.println("Game Over");
+                    System.out.println("The word was " + wordToGuess);
+                    keepGoing = false;
+                } else {
+                    System.out.println("You have " + lives + " remaining lives");
+                    System.out.println("Try again");
+                    System.out.println();
+                }
+            } else {
+                if (!guessedWord.toString().contains("_")) {
+                    System.out.println("You got it !!!");
+                    keepGoing = false;
+                } else {
+                    System.out.println("Great!!! Keep going");
+                }
+            }
+
+        } //end while
+    } //end main
+} //end class
